cc7c64bddd1a04a07cd0b8f952d53776d10efc7c
Jairo Santos <jairodantesantos27@gmail.com>
2025-09-10 18:06:28 -0700

    Test commit

---
+++ calculator.py
@@ -0,0 +1,17 @@
+operation = input("Enter an operation (+ - / *): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if operation == "+":
+    result = num1 + num2
+
+if operation == "-":
+    result = num1 - num2
+
+if operation == "/":
+    result = num1 / num2
+
+if operation == "*":
+    result = num1 * num2
+
+print("Hello world")
